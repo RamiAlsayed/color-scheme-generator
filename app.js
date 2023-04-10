cb6ba2dbace4edc4e3535e4129f58703a6c1ce8f
Rami Alsayed <58389689+RamiAlsayed@users.noreply.github.com>
2023-04-10 22:44:42 +0300

    Add script files

---
+++ app.js
@@ -0,0 +1,47 @@
+const colorInput = document.querySelector('#seed-color');
+const predefinedColors = document.querySelector('#predefined-colors');
+const form = document.querySelector('#form');
+const colorData = document.querySelector('.color-data');
+const hexCode = document.querySelector('.hex-code');
+
+function getColor() {
+  form.addEventListener('submit', (e) => {
+    const selectedColor = colorInput.value.substring(1);
+    const predeColors = predefinedColors.value;
+
+    const info = {
+      name: selectedColor,
+      mode: predeColors,
+    };
+
+    let html = ``;
+    let htmlHex = ``;
+
+    fetch(
+      `https://www.thecolorapi.com/scheme?named=false&hex=${info.name}&mode=${info.mode}&count=5`
+    )
+      .then((req) => req.json())
+      .then((data) => {
+        let dataArry = data.colors;
+
+        dataArry.map((colorHex) => {
+          htmlHex += `<div class="hex-value"> ${colorHex.hex.value}</div>`;
+        });
+
+        html = `
+           
+             <div class="img-box"><img class="imgo" src="${data.image.named}" alt="color"></div>
+             
+      
+               
+                `;
+
+        colorData.innerHTML = html;
+        hexCode.innerHTML = htmlHex;
+      });
+
+    e.preventDefault();
+  });
+}
+
+getColor();
